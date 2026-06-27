a21892c002e1bdd280900ea4a91eb2e04becf89f
Ruhulfile <ruhulrock2020@gmail.com>
2026-06-28 00:30:01 +0530

    Implement login button functionality
    Add login functionality with username and password validation.

--- login.js
+++ login.js
@@ -1,1 +1,20 @@
+const loginBtn = document.getElementById("loginBtn");
 
+loginBtn.addEventListener("click", function () {
+
+    const username = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+    if (username === "rahim" && password === "123456") {
+
+        alert("Login Successful ✅");
+
+        window.location.href = "dashboard.html";
+
+    } else {
+
+        alert("Wrong Username or Password ❌");
+
+    }
+
+});
