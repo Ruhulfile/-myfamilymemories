aaf1f665af814d1351d635d7a4bd05a21c076c9e
Ruhulfile <ruhulrock2020@gmail.com>
2026-07-02 18:53:14 +0530

    Update cloudinary.js

--- js/cloudinary.js
+++ js/cloudinary.js
@@ -1,1 +1,41 @@
+// =======================================
 
+// My Family Memories - Cloudinary v3
+
+// =======================================
+
+// Cloudinary Details
+
+export const CLOUD_NAME = "i6jigbyf";
+
+export const UPLOAD_PRESET = "family-memories";
+
+// Upload Function
+
+export async function uploadImage(file){
+
+    const data = new FormData();
+
+    data.append("file", file);
+
+    data.append("upload_preset", UPLOAD_PRESET);
+
+    const response = await fetch(
+
+        `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`,
+
+        {
+
+            method:"POST",
+
+            body:data
+
+        }
+
+    );
+
+    const result = await response.json();
+
+    return result.secure_url;
+
+}
